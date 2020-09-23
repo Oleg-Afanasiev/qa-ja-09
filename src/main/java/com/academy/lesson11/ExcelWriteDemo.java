8943340eefe6ef4cd95722d1d26a8a5a1d4a8ae3
oleg <oleg.kh81@gmail.com>
2020-09-24 01:26:19 +0300

    excel demo

---
+++ src/main/java/com/academy/lesson11/ExcelWriteDemo.java
@@ -0,0 +1,29 @@
+package com.academy.lesson11;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ExcelWriteDemo {
+    public static void main(String[] args) {
+        try(
+                OutputStream os = new FileOutputStream("d:/tmp/demo/table.xlsx");
+                Workbook workbook = new SXSSFWorkbook();
+        ) {
+            Sheet sheet = workbook.createSheet("sheet-demo");
+            Row row = sheet.createRow(1);
+            Cell cell1 = row.createCell(0);
+            cell1.setCellValue(new XSSFRichTextString("Hello"));
+            Cell cell2 = row.createCell(1);
+            cell2.setCellValue(123);
+
+            workbook.write(os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
