a954bb52b24d206fd5f14752d0275d3ba142d333
oleg <oleg.kh81@gmail.com>
2020-10-01 21:24:43 +0300

    test cfg

--- src/test/java/com/academy/automation/LoginTests.java
+++ src/test/java/com/academy/automation/LoginTests.java
@@ -1,30 +1,44 @@
 package com.academy.automation;
 
-import java.util.regex.Pattern;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
 public class LoginTests {
     private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        baseUrl = "https://www.google.com/";
+        String path = System.getProperty("cfg");
+        System.out.println(path);
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(path));
+        String chromeDriver = properties.getProperty("driver.chrome");
+        System.out.println(chromeDriver);
+
+        System.setProperty("webdriver.chrome.driver", "D:/programming/teaching/qa-09-maven/drivers/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "D:/programming/teaching/qa-09-maven/drivers/geckodriver.exe");
+        System.setProperty("webdriver.edge.driver", "D:/programming/teaching/qa-09-maven/drivers/msedgedriver.exe");
+        driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testUntitledTestCase() throws Exception {
+    public void testLoginError() throws Exception {
         driver.get("http://automationpractice.com/index.php");
         driver.findElement(By.linkText("Sign in")).click();
         driver.findElement(By.id("email")).click();
@@ -34,8 +48,10 @@ public class LoginTests {
         driver.findElement(By.id("passwd")).sendKeys("password");
         driver.findElement(By.xpath("//button[@id='SubmitLogin']/span")).click();
         assertEquals(driver.findElement(By.xpath("//div[@id='center_column']/div/ol/li")).getText(), "Invalid email address.");
-        // ERROR: Caught exception [unknown command []]
-        // ERROR: Caught exception [unknown command []]
+    }
+
+    @Test
+    public void testLoginSuccess() {
     }
 
     @AfterClass(alwaysRun = true)
@@ -44,39 +60,6 @@ public class LoginTests {
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
         }
     }
 }
