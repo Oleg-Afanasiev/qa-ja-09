1a0a230ca5aa0fa87ba7589895aedee2edaad86e
oleg <oleg.kh81@gmail.com>
2020-10-06 21:30:27 +0300

    test cfg

--- src/test/java/com/academy/automation/AutomationTests.java
+++ src/test/java/com/academy/automation/AutomationTests.java
@@ -3,66 +3,41 @@ package com.academy.automation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
-public class LoginTests {
+public class AutomationTests {
     private WebDriver driver;
     private StringBuffer verificationErrors = new StringBuffer();
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        String path = System.getProperty("cfg");
-        System.out.println(path);
-
-        Properties properties = new Properties();
-        properties.load(new FileInputStream(path));
-        String chromeDriver = properties.getProperty("driver.chrome");
-        System.out.println(chromeDriver);
 
         System.setProperty("webdriver.chrome.driver", "D:/programming/teaching/qa-09-maven/drivers/chromedriver.exe");
         System.setProperty("webdriver.gecko.driver", "D:/programming/teaching/qa-09-maven/drivers/geckodriver.exe");
         System.setProperty("webdriver.edge.driver", "D:/programming/teaching/qa-09-maven/drivers/msedgedriver.exe");
-        driver = new EdgeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testLoginError() throws Exception {
-        driver.get("http://automationpractice.com/index.php");
-        WebElement signInLink = driver.findElement(By.linkText("Sign in"));
-        signInLink.click();
-
-        driver.findElement(By.id("email")).click();
-        driver.findElement(By.id("email")).clear();
-        driver.findElement(By.id("email")).sendKeys("username");
-        driver.findElement(By.id("passwd")).clear();
-        driver.findElement(By.id("passwd")).sendKeys("password");
-        driver.findElement(By.xpath("//button[@id='SubmitLogin']/span")).click();
-        assertEquals(driver.findElement(By.xpath("//div[@id='center_column']/div/ol/li")).getText(), "Invalid email address.");
-    }
-
-    @Test
-    public void testLoginErrorCase2() {
-
-    }
-
-    @Test
-    public void testLoginSuccess() {
+    public void testSelectWomenDress() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+        WebElement selectBox = driver.findElement(By.id("selectProductSort"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.id("menu")));
     }
 
     @AfterClass(alwaysRun = true)
---
+++ src/test/java/com/academy/automation/CitrusTests.java
@@ -0,0 +1,85 @@
+package com.academy.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class CitrusTests {
+
+    private WebDriver driver;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+//        String path = System.getProperty("cfg");
+//        System.out.println(path);
+//
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream(path));
+//        String chromeDriver = properties.getProperty("driver.chrome");
+//        System.out.println(chromeDriver);
+
+        System.setProperty("webdriver.chrome.driver", "D:/programming/teaching/qa-09-maven/drivers/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "D:/programming/teaching/qa-09-maven/drivers/geckodriver.exe");
+        System.setProperty("webdriver.edge.driver", "D:/programming/teaching/qa-09-maven/drivers/msedgedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testTvSelect() {
+        System.out.println("Citrus test");
+        driver.get("https://www.citrus.ua");
+        String menuXpath = "//div/ul/li[@class='menu-aim__item']/a[@href='/televizory-photo-video/']";
+        String tvLinkXPath = "//*[@id='header']/div[3]/div/div[2]/div[2]/ul/li[3]/div/div/div[2]/ul/li[2]/a";
+        String pricesXPath = "//div[@class='prices__price']/span";
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath(menuXpath)))
+                .build()
+                .perform();
+
+        driver.findElement(By.xpath(tvLinkXPath)).click();
+        List<WebElement> prices = driver.findElements(By.xpath(pricesXPath));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 1 - for
+        for(int i = 0; i < prices.size(); i++) {
+            System.out.println(prices.get(i).getText());
+        }
+        // 2 - stream API
+       // prices.stream().map(WebElement::getText).forEach(System.out::println);
+
+        // Не торопимся закрывать
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+}
