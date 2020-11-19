eca14e78f66b208a7c6965d7662b75ebfa297af1
af_oleg <oleg.kh81@gmail.com>
2020-11-19 20:08:37 +0200

    lesson 21 - login tests init

--- src/test/java/com/academy/automation/selenide/LoginTests.java
+++ src/test/java/com/academy/automation/selenide/LoginTests.java
@@ -1,7 +1,12 @@
 package com.academy.automation.selenide;
 
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,14 +20,27 @@ public class LoginTests {
         Configuration.timeout = 10;
     }
 
-    @Test
-    public void testLoginError() {
+    @Test(dataProvider = "loginErrorProvider")
+    public void testLoginError(String username, String password, String expectedMsg) {
         open(Configuration.baseUrl);
-        closeWindow();
+        $(By.linkText("Sign in")).click();
+        $(By.id("email")).sendKeys(username);
+        $(By.id("passwd")).sendKeys(password);
+        $(By.xpath("//button[@id='SubmitLogin']/span")).click();
+        String actualMsg = $(By.xpath("//div[@id='center_column']/div/ol/li")).getText();
+
+        Assert.assertEquals(actualMsg, expectedMsg);
     }
 
     @AfterClass
     public void tearDown() {
+        closeWindow();
+    }
 
+    @DataProvider
+    public Object[][] loginErrorProvider() {
+        return new Object[][] {
+                {"user","123","Invalid email address."}
+        };
     }
 }
